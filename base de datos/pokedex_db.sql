13d5eedbc73e1644eff4df268ff70a06a6190810
Javier <cabreramoreljavier@gmail.com>
2024-09-17 19:48:01 -0300

    base de datos

---
+++ base de datos/pokedex_db.sql
@@ -0,0 +1,120 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 14-09-2024 a las 23:13:11
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `pokedex_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `pokemon`
+--
+
+CREATE TABLE pokemon (
+  id_pokemon int(4) NOT NULL AUTO_INCREMENT,
+  uuid_pokemon varchar(13) NOT NULL,
+  img_pokemon varchar(256) DEFAULT NULL,
+  nombre_pokemon varchar(64) NOT NULL,
+  descripcion_pokemon varchar(512) DEFAULT NULL,
+  id_tipo int(2) NOT NULL,
+  PRIMARY KEY (id_pokemon)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+--
+-- Volcado de datos para la tabla `pokemon`
+--
+
+INSERT INTO `pokemon` (`uuid_pokemon`, `img_pokemon`, `nombre_pokemon`, `descripcion_pokemon`, `id_tipo`) VALUES
+('66e5fa249fa49', 'imagenes/charmander.png', 'Charmander', 'Charmander es un pequeño lagarto bípedo. Sus características de fuego son resaltadas por su color de piel anaranjado y su cola con la punta envuelta en llamas.', 1),
+('bf3d2a769acd4', 'imagenes/bulbasaur.png', 'Bulbasaur', 'Bulbasaur tiene la habilidad de absorber energía solar gracias a la planta en su espalda. Es de tipo Planta y se caracteriza por su relación con la naturaleza.', 2),
+('9cfa3a68b5df1', 'imagenes/squirtle.png', 'Squirtle', 'Squirtle es un Pokémon tipo Agua. Su caparazón sirve para protegerse de ataques y su capacidad de disparar agua es destacada.', 4),
+('1bcd3e4978e61', 'imagenes/geodude.png', 'Geodude', 'Geodude es de tipo Tierra y Roca. Su cuerpo duro le permite resistir golpes, y su afinidad por las montañas lo hace difícil de encontrar en zonas llanas.', 3),
+('f1a94c6473f18', 'imagenes/charizard.png', 'Charizard', 'Charizard es la evolución final de Charmander. Este Pokémon de tipo Fuego y Volador tiene la capacidad de lanzar potentes llamaradas.', 1),
+('cc5e9f2687e13', 'imagenes/venusaur.png', 'Venusaur', 'Venusaur es la evolución final de Bulbasaur. Su gran tamaño y su planta en la espalda le permiten utilizar el poder de la naturaleza a su favor.', 2),
+('be9d4b6d7c4a9', 'imagenes/blastoise.png', 'Blastoise', 'Blastoise es la evolución final de Squirtle. Sus cañones de agua en el caparazón le permiten lanzar chorros de agua a gran presión.', 4),
+('af5c3d8712bf2', 'imagenes/onyx.png', 'Onix', 'Onix es un Pokémon de tipo Tierra y Roca. Su cuerpo gigante de piedras le da gran resistencia y su capacidad de excavar lo hace imparable bajo tierra.', 3),
+('ec7f2b1973cd8', 'imagenes/vulpix.png', 'Vulpix', 'Vulpix es un pequeño Pokémon de tipo Fuego. Sus seis colas le otorgan gran flexibilidad y sus ataques de fuego pueden variar en intensidad.', 1);
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tipo`
+--
+
+CREATE TABLE `tipo` (
+  `id_tipo` int(2) NOT NULL,
+  `descripcion_tipo` varchar(32) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `tipo`
+--
+
+INSERT INTO `tipo` (`descripcion_tipo`) VALUES
+('Fuego'),
+('Agua'),
+('Planta'),
+('Veneno');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `pokemon`
+--
+ALTER TABLE `pokemon`
+  ADD PRIMARY KEY (`id_pokemon`),
+  ADD KEY `id_tipo` (`id_tipo`);
+
+--
+-- Indices de la tabla `tipo`
+--
+ALTER TABLE `tipo`
+  ADD PRIMARY KEY (`id_tipo`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `pokemon`
+--
+ALTER TABLE `pokemon`
+  MODIFY `id_pokemon` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `tipo`
+--
+ALTER TABLE `tipo`
+  MODIFY `id_tipo` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `pokemon`
+--
+ALTER TABLE `pokemon`
+  ADD CONSTRAINT `pokemon_ibfk_1` FOREIGN KEY (`id_tipo`) REFERENCES `tipo` (`id_tipo`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- index.php
+++ index.php
@@ -34,13 +34,44 @@
         <thead>
         <tr>
             <th>Imagen</th>
-            <th>Nombre</th>
-            <th>Número</th>
             <th>Tipo</th>
+            <th>Número</th>
+            <th>Nombre</th>
         </tr>
         </thead>
         <tbody>
-        <!-- Aquí se cargarán los resultados de la búsqueda -->
+        <?php
+
+        $host = 'localhost';
+        $dbname = 'pokedex_db';
+        $username = 'root';
+        $password = '';
+
+        try {
+            $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+
+            $query = $pdo->query("SELECT * FROM pokemon");
+
+
+            $resultados = $query->fetchAll(PDO::FETCH_ASSOC);
+
+
+
+            for ($i = 0; $i < count($resultados); $i++) {
+                echo "<tr>";
+                echo "<td><img src='" . ($resultados[$i]['img_pokemon']) . "' alt='" . "' width='80' height='80'></td>";
+                echo "<td><img src='" . "tipo/tipo" . ($resultados[$i]['id_tipo']) . ".png"  . "' alt='" . "' width='50' height='50'></td>";
+                echo "<td>" . ($resultados[$i]['id_pokemon']) . "</td>";
+                echo "<td>" . ($resultados[$i]['nombre_pokemon']) . "</td>";
+                echo "</tr>";
+            }
+
+        } catch (PDOException $e) {
+            echo "<tr><td colspan='4'>Error de conexión: " . $e->getMessage() . "</td></tr>";
+        }
+        ?>
+
+
         </tbody>
     </table>
 </div>
