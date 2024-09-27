a944bb31abae9936f26c7f37cb894802bf0fbee9
alexisvh5 <109443139+alexisvh5@users.noreply.github.com>
2024-09-27 00:31:25 -0300

    cambio estilos index

--- index.php
+++ index.php
@@ -20,7 +20,7 @@ $pokeNegocio = new PokemonNegocio($db);
     <title>Pokedex Search</title>
 
     <link rel="stylesheet" href="estilos/style.css">
-    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link rel="stylesheet" href="estilos/vista-admin.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
 <body>
